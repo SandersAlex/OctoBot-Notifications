4cb981afdc6eb1a77b5e5bf247f275a544ad80fc
Herklos <herklos.dev@protonmail.com>
2019-07-24 12:24:11 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,53 @@
+#  Drakkar-Software OctoBot-Notifications
+#  Copyright (c) Drakkar-Software, All rights reserved.
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3.0 of the License, or (at your option) any later version.
+#
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library.
+# from distutils.extension import Extension
+from setuptools import find_packages
+from setuptools import setup
+
+from octobot_notifications import PROJECT_NAME, VERSION
+
+PACKAGES = find_packages(exclude=["tests"])
+# long description from README file
+with open('README.md', encoding='utf-8') as f:
+    DESCRIPTION = f.read()
+
+REQUIRED = open('requirements.txt').read()
+REQUIRES_PYTHON = '>=3.7'
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    url='https://github.com/Drakkar-Software/OctoBot-Notifications',
+    license='LGPL-3.0',
+    author='Drakkar-Software',
+    author_email='drakkar-software@protonmail.com',
+    description='OctoBot project notifications package',
+    packages=PACKAGES,
+    include_package_data=True,
+    long_description=DESCRIPTION,
+    install_requires=REQUIRED,
+    tests_require=["pytest"],
+    test_suite="tests",
+    zip_safe=False,
+    data_files=[],
+    setup_requires=[],
+    python_requires=REQUIRES_PYTHON,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Cython',
+    ],
+)
